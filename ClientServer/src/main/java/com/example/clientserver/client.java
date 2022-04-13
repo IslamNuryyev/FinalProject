15a671247990f8462897e7742779b68f87844ca1
Islam Nuryyev <islamn2578@gmail.com>
2022-04-12 23:46:10 -0400

    logic

--- ClientServer/src/main/java/com/example/clientserver/client.java
+++ ClientServer/src/main/java/com/example/clientserver/client.java
@@ -11,6 +11,12 @@ public class client{
         try (Socket sock = new Socket("localhost", 3000)){
             System.out.println("Connected to server...");
             System.out.println("Input \"you are done\" to terminate connection...");
+
+            System.out.println("----------------------------------");
+            System.out.println("Welcome to Battleship Lite!");
+            System.out.println("----------------------------------\n");
+
+            System.out.println("Select the location of your battleship [0,9]: ");
             //get input from the user to send as a message
             PrintWriter dout = new PrintWriter(sock.getOutputStream(), true);
             Scanner scanner = new Scanner(System.in);
@@ -19,6 +25,7 @@ public class client{
             while(!message.equals("you are done")){
                 message = scanner.nextLine();
                 dout.println(message);
+                break;
             }
             scanner.close();
         }
--- ClientServer/src/main/java/com/example/clientserver/server.java
+++ ClientServer/src/main/java/com/example/clientserver/server.java
@@ -7,12 +7,12 @@ import java.util.*;
 public class server {
     private static int player1location = -1;
     private static int player2location = -1;
-    private int player1guess;
-    private int player2guess;
+    private static int player1guess;
+    private static int player2guess;
 
-    private int max = 9;
-    private int round = 1;
-    private char user_inquiry;
+    private static int max = 9;
+    private static int round = 1;
+    private static char user_inquiry;
 
     private static class ClientHandler implements Runnable {
 
@@ -27,21 +27,41 @@ public class server {
             try {
                 inStream = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
                 String message;
+//                message = inStream.readLine();
+//                player1location = Integer.parseInt(message);
+//
+//                message = inStream.readLine();
+//                player2location = Integer.parseInt(message);
+//
+//                System.out.println( "player1location = " + player1location);
+//                System.out.println( "player2location =  " + player2location);
+
 
                 while ((message = inStream.readLine()) != null) {
                     if (player1location == -1) {
                         player1location = Integer.parseInt(message);
-                    } else {
-                        player2location = Integer.parseInt(message);
-                    }
+                        System.out.println( "Player 1 has selected its' location");
 
-//
+                    } else if (player2location == -1) {
+                        player2location = Integer.parseInt(message);
+                        System.out.println( "Player 2 has selected its' location");
+                        break;
+                    }
+                }
+
+//                for development
                 System.out.println( "player1location = " + player1location);
                 System.out.println( "player2location =  " + player2location);
-//                System.out.println("firstInputted true = " + this.firstInputted);
+//                for development
 
-//
-                }
+
+                System.out.println("Ready to begin ");
+                System.out.println("Round " + round);
+
+
+
+
+
             }
             catch(IOException e){
                 e.printStackTrace();
@@ -61,17 +81,12 @@ public class server {
     public static void main(String[] args){
         ServerSocket serve = null;
 
-
-        System.out.println("----------------------------------");
-        System.out.println("Welcome to Battleship Lite!");
-        System.out.println("----------------------------------\n");
-
-
         try {
             serve = new ServerSocket(3000); //0 -> lets your OS select a port; port > 1024
             serve.setReuseAddress(true);
             System.out.println("Starting server...");
             System.out.println("Waiting for client connection...");
+
             while(true){
                 Socket sock = serve.accept();
                 System.out.println("Client is connected " + sock.getInetAddress().getHostAddress()); //this will display the host address of client
